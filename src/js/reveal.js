c9085b78921326cc9128f17f8d130ed739b1752b
luiz <luiz308araujofarias@gmail.com>
2024-11-02 19:53:24 -0300

    Primeira versão

---
+++ src/js/reveal.js
@@ -0,0 +1,174 @@
+//Reset//
+window.revelar = ScrollReveal({reset:true})
+
+//vertical//
+revelar.reveal('.vertical-t',{
+    duration: 1000,
+    distance: '90px',
+    origin: 'top'
+})
+//vetical cima pra baixo com delay//
+revelar.reveal('.vertical-td',{
+    duration: 1000,
+    distance: '90px',
+    origin: 'top',
+    delay: 1000
+})
+//vetical cima pra baixo com delay2//
+revelar.reveal('.vertical-td2',{
+    duration: 1000,
+    distance: '110px',
+    origin: 'top',
+    delay: 1200
+})
+//vetical cima pra baixo com delay3//
+revelar.reveal('.vertical-td3',{
+    duration: 1000,
+    distance: '130px',
+    origin: 'top',
+    delay: 1400
+})
+//vetical cima pra baixo com delay4//
+revelar.reveal('.vertical-td4',{
+    duration: 1000,
+    distance: '150px',
+    origin: 'top',
+    delay: 1600
+})
+//vetical cima pra baixo com delay5//
+revelar.reveal('.vertical-td5',{
+    duration: 1000,
+    distance: '170px',
+    origin: 'top',
+    delay: 1800
+})
+
+
+//debaixo pra cima//
+revelar.reveal('.vertical-b',{
+    duration: 1000,
+    distance: '90px',
+    origin: 'bottom',
+})
+//vertical debaixo pra cima com delay//
+revelar.reveal('.vertical-bd',{
+    duration: 1000,
+    distance: '90px',
+    origin: 'bottom',
+    delay: 1100
+})
+//vertical debaixo pra cima com delay2//
+revelar.reveal('.vertical-bd2',{
+    duration: 1000,
+    distance: '90px',
+    origin: 'bottom',
+    delay: 1300
+})
+//vertical debaixo pra cima com delay3//
+revelar.reveal('.vertical-bd3',{
+    duration: 1000,
+    distance: '90px',
+    origin: 'bottom',
+    delay: 1500
+})
+//vertical debaixo pra cima com delay4//
+revelar.reveal('.vertical-bd4',{
+    duration: 1000,
+    distance: '90px',
+    origin: 'bottom',
+    delay: 1700
+})
+//vertical debaixo pra cima com delay5//
+revelar.reveal('.vertical-bd5',{
+    duration: 1000,
+    distance: '90px',
+    origin: 'bottom',
+    delay: 1900
+})
+
+//laterais//
+
+//esquerda pra direita com delay//
+revelar.reveal('.lateral-ld',{
+    duration: 1000,
+    distance: '90px',
+    origin: 'left',
+    delay: 1005
+})
+//esquerda pra direita com delay2//
+revelar.reveal('.lateral-ld2',{
+    duration: 1000,
+    distance: '125px',
+    origin: 'left',
+    delay: 1006
+})
+//esquerda pra direita com delay3//
+revelar.reveal('.lateral-ld3',{
+    duration: 1000,
+    distance: '125px',
+    origin: 'left',
+    delay: 1007
+})
+//esquerda pra direita com delay4//
+revelar.reveal('.lateral-ld4',{
+    duration: 1000,
+    distance: '125px',
+    origin: 'left',
+    delay: 1008
+})
+//esquerda pra direita com delay 5//
+revelar.reveal('.lateral-ld5',{
+    duration: 1000,
+    distance: '125px',
+    origin: 'left',
+    delay: 1009
+})
+
+
+//direita pra esquerda com delay//
+revelar.reveal('.lateral-rd',{
+    duration: 1000,
+    distance: '90px',
+    origin: 'right',
+    delay: 1005
+})
+//direita pra esquerda com delay2//
+revelar.reveal('.lateral-rd2',{
+    duration: 1000,
+    distance: '125px',
+    origin: 'right',
+    delay: 1006
+})
+//direita pra esquerda com delay3//
+revelar.reveal('.lateral-rd3',{
+    duration: 1000,
+    distance: '125px',
+    origin: 'right',
+    delay: 1007
+})
+//direita pra esquerda com delay4//
+revelar.reveal('.lateral-rd4',{
+    duration: 1000,
+    distance: '125px',
+    origin: 'right',
+    delay: 1008
+})
+//direita pra esquerda com delay5//
+revelar.reveal('.lateral-rd5',{
+    duration: 1000,
+    distance: '125px',
+    origin: 'right',
+    delay: 1009
+})
+//direita pra esquerda//
+revelar.reveal('.lateral-r',{
+    duration: 1000,
+    distance: '90px',
+    origin: 'right'
+})
+//esquerda esquerda//
+revelar.reveal('.lateral-l',{
+    duration: 1000,
+    distance: '120px',
+    origin: 'left'
+})
---
+++ src/js/script.js
@@ -0,0 +1,21 @@
+//tela de carregamento//
+window.onload = function(){
+    const loading = document.querySelector('.loading')
+    let body = document.querySelector('body')
+    body.classList.add('no-scroll')
+
+    setTimeout(() => {
+        loading.style.opacity = '0'
+        setTimeout(() => {
+            loading.style.display = 'none'
+            body.classList.remove('no-scroll')
+        },500)
+    },2000)
+}
+
+
+//barra de navegação//
+window.addEventListener('scroll', ()=> {
+    let navbar = document.querySelector('header nav')
+    navbar.classList.toggle('rolagem', window.scrollY > 800)
+})
